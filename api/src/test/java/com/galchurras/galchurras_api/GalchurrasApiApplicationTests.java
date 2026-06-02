a5a25eef8e0678426c9843b4a5952679ca656e80
Gabriel <gabrielgpace@hotmail.com>
2026-06-02 00:04:49 -0300

    Initial commit

---
+++ api/src/test/java/com/galchurras/galchurras_api/GalchurrasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galchurras.galchurras_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalchurrasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
